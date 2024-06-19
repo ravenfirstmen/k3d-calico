b458c23c13013a21fd84ace737967b47b17a2237
aacs <antonio.costa.salgado@gmail.com>
2024-06-19 18:37:45 +0100

    Updated cluuster config

--- docker-registry-start.sh
+++ docker-registry-start.sh
@@ -4,3 +4,5 @@ export REGISTRY_NAME="k3d-registry.localhost"
 export REGISTRY_PORT="5000"
 
 docker run -d -p 5000:5000 --restart=always --name k3d-registry.localhost -v "$(pwd)/docker-registry.yaml:/etc/docker/registry/config.yml" registry:2
+
+# docker network connect k3d-k3s-default k3d-registry.localhost
