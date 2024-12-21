c1707e230f2a9e64ace061f2bd37516d06ad2ee6
aacs <antonio.costa.salgado@gmail.com>
2024-12-21 20:07:34 +0000

    Update istio config

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+uuidgen -r -x | tr -d '-' > $(pwd)/machine-id
+
 export K3D_FIX_MOUNTS=1
 k3d cluster create --config cluster-config.yaml 
--- gloo-istio/istio/99-dump-envoy-config.sh
+++ gloo-istio/istio/99-dump-envoy-config.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-# kubectl exec istio-ingress-6ff5d5b79d-6q4g6 -n istio-system -it -- curl 'http://localhost:15000/config_dump'
+gateway=$(kubectl get pods -l app=istio-ingress --namespace istio-system -o json | jq -r '.items[].metadata.name')
+
+# dump istio gateway config 
+kubectl exec ${gateway} -n istio-system -it -- curl 'http://localhost:15000/config_dump'
 
 # Verify rate limiter cluster is properly registered
-# istioctl proxy-config cluster istio-ingress-6ff5d5b79d-6q4g6.istio-system -o json
+istioctl proxy-config cluster ${gateway}.istio-system -o json
 
 # check rate limiter filter is attached to the http filter chain
-# istioctl proxy-config listener istio-ingress-6ff5d5b79d-6q4g6.istio-system -o json
+istioctl proxy-config listener ${gateway}.istio-system -o json
 
 # Verify the route configuration 
-# istioctl proxy-config route istio-ingress-6ff5d5b79d-6q4g6.istio-system -o json
+istioctl proxy-config route ${gateway}.istio-system -o json
